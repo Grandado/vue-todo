302e7e1109ddd9469a6bb4048eaf04ddae0e4af4
Guilherme Nascimento <57995773+Grandado@users.noreply.github.com>
2022-10-13 15:31:49 -0300

    upload vue-todo

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-list-item
+      :class="{ 'blue lighten-4': tarefa.concluido }"
+      @click="tarefaRes.concluido = !tarefaRes.concluido"
+    >
+      <template v-slot:default="{}">
+        <v-list-item-action>
+          <v-checkbox :input-value="tarefa.concluido"></v-checkbox>
+        </v-list-item-action>
+
+        <v-list-item-content>
+          <v-list-item-title
+            :class="{ 'text-decoration-line-through': tarefa.concluido }"
+            >{{ tarefa.titulo }}</v-list-item-title
+          >
+        </v-list-item-content>
+
+        <v-list-item-action icon @click.stop="handleRemoverTarefa(tarefa.id)">
+          <v-btn icon>
+            <v-icon color="red darken-3">mdi-trash-can</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </template>
+    </v-list-item>
+    <v-divider></v-divider>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { tarefaRes: this.tarefa };
+  },
+  props: ['tarefa'],
+
+  methods: {
+    handleRemoverTarefa(id) {
+      this.$store.commit('removerTarefa', id);
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,30 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    tarefas: [
+      { id: 1, titulo: 'Ir ao mercado', concluido: false },
+      { id: 2, titulo: 'Comprar Ração', concluido: false },
+    ],
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    adicionarTarefa(state, titulo) {
+      if (titulo) {
+        state.tarefas.push({
+          id: new Date().getTime(),
+          titulo,
+          concluido: false,
+        });
+      }
+    },
+    removerTarefa(state, id) {
+      state.tarefas = state.tarefas.filter((e) => e.id !== id);
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
